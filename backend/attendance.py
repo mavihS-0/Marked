2850d8bc5f9af1f0566ef0a01bf6fecec83537ab
mavihS-0 <shivam9753singh@gmail.com>
2024-04-10 22:58:09 +0530

    renamed

--- backend/attendance.py
+++
@@ -1,9 +0,0 @@
-from face_encodings import get_face_encodings
-from write_attendance import mark_attendance
-
-image_names, known_face_encoding = get_face_encodings()
-
-print("Student names:", image_names)
-print("Face encodings:", known_face_encoding)
-
-mark_attendance(image_names, known_face_encoding)
